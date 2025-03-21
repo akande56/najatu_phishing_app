b00a7f57ee46adf9d0fbbe72663fe9a5c21f41e9
akande56 <abdulsalamabubakar52@gmail.com>
2025-03-21 03:48:34 +0100

    again

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-echo python manage.py makemigrations --merge
+echo python manage.py makemigrations 
 python manage.py migrate
 # echo "from django.contrib.auth     import get_user_model; User= get_user_model(); User.objects.create_superuser('Admin','Admin', 'pass123')"| python manage.py shell
