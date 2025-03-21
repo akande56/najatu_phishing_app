8c2f08067f9923123b6f0fe8f50420d65be40d9b
akande56 <abdulsalamabubakar52@gmail.com>
2025-03-21 03:22:14 +0100

    fix allauth user configuration

--- config/settings/base.py
+++ config/settings/base.py
@@ -273,13 +273,14 @@ ACCOUNT_ALLOW_REGISTRATION = env.bool("DJANGO_ACCOUNT_ALLOW_REGISTRATION", True)
 # https://docs.allauth.org/en/latest/account/configuration.html
 ACCOUNT_LOGIN_METHODS = {"email"}
 # https://docs.allauth.org/en/latest/account/configuration.html
-ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_REQUIRED = True   
+ACCOUNT_SIGNUP_FIELDS = ['email*', 'password1*', 'password2*']  
 # https://docs.allauth.org/en/latest/account/configuration.html
 ACCOUNT_USERNAME_REQUIRED = False
 # https://docs.allauth.org/en/latest/account/configuration.html
 ACCOUNT_USER_MODEL_USERNAME_FIELD = None
 # https://docs.allauth.org/en/latest/account/configuration.html
-ACCOUNT_EMAIL_VERIFICATION = "mandatory"
+ACCOUNT_EMAIL_VERIFICATION = "none"
 # https://docs.allauth.org/en/latest/account/configuration.html
 ACCOUNT_ADAPTER = "najatu_ai.users.adapters.AccountAdapter"
 # https://docs.allauth.org/en/latest/account/forms.html
@@ -290,5 +291,3 @@ SOCIALACCOUNT_ADAPTER = "najatu_ai.users.adapters.SocialAccountAdapter"
 SOCIALACCOUNT_FORMS = {"signup": "najatu_ai.users.forms.UserSocialSignupForm"}
 
 
-# Your stuff...
-# ------------------------------------------------------------------------------
